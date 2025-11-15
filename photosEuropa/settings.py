d099f44bda10e7cbec144aef28308f1387a987eb
calberto <ninho.silva@email.com>
2025-11-14 23:07:26 -0300

    atualização para o banco PostgreSQL

--- photosEuropa/settings.py
+++ photosEuropa/settings.py
@@ -80,6 +80,15 @@ DATABASES = {
     )
 }
 """
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -90,13 +99,15 @@ DATABASES = {
         'PORT': os.environ.get('PGPORT', '5432'),
     }
 }
-
+"""
 
 
 # Fallback local (opcional)
+"""
 if os.getenv('DATABASE_URL'):
     import dj_database_url
     DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
+"""
 
 # Configuração do Cloudinary
 CLOUDINARY_STORAGE = {
